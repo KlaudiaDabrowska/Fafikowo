06102644dfe295c3cec64610c9095de2a51ec116
KlaudiaDabrowska <kosaa96@gmail.com>
2025-07-14 22:38:56 +0200

    setup mantine

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+
+import "@mantine/core/styles.css";
+import { theme } from "@/styles/theme";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +28,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <ColorSchemeScript />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <MantineProvider theme={theme}>{children}</MantineProvider>
       </body>
     </html>
   );
